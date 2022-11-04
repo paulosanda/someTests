b9b99c1d6171fbac129f41ee8eb2a1ff1a16fc98
paulosanda <paulosanda@gmail.com>
2022-11-04 11:47:12 -0300

    command created

---
+++ app/Console/Commands/CreateDailyLog.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CreateDailyLog extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'daily-log';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'input in users validate';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return 0;
+    }
+}
