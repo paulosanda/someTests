1f4d4305cf23d63bb7045fb5cbad519b128cf85f
paulosanda <paulosanda@gmail.com>
2022-11-15 11:59:38 -0300

    feat/password changed notification

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -3,6 +3,8 @@
 namespace App\Observers;
 
 use App\Models\User;
+use App\Notifications\PasswordChanged;
+use Illuminate\Support\Facades\Log;
 
 class UserObserver
 {
@@ -25,7 +27,9 @@ class UserObserver
      */
     public function updated(User $user)
     {
-        //
+        if ($user->isDirty('password')) {
+            $user->notify(new PasswordChanged());
+        }
     }
 
     /**
